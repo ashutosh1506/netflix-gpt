cebebbeb8f97d2586074273498ae6f1572745756
Ashutosh Gupta <guptaashutosh502@gmail.com>
2025-06-25 14:08:40 +0530

    Updated CSS

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -5,7 +5,7 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies)
   return (
     <div className=" bg-gray-950 ">
-      <div className="mt-0 pt-40 md:-mt-62 md:pt-0 md:pl-6 relative z-20">
+      <div className="mt-0 pt-40 md:-mt-60  md:pt-0 md:pl-6 relative z-20">
         <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
         <MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />
         <MovieList title={"Popular"} movies={movies?.popularMovies} />
